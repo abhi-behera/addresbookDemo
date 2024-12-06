c48d38bfde271d3b4bc7bf1b8787da9ebb1eec9f
abhi-behera <abhijitbehera1997@gmail.com>
2024-12-06 12:55:51 +0530

    Code optimization : - in place of fetching from json data, form model are introduced. - provider not implemented yet.

--- lib/provider/formfield_provider.dart
+++ lib/provider/formfield_provider.dart
@@ -1,20 +1,41 @@
-import 'package:dumyapp1/model/formfield_model.dart';
-import 'package:flutter/material.dart';
+// import 'package:dumyapp1/model/formfield_model.dart';
+// import 'package:flutter/material.dart';
 
-class FormProvider extends ChangeNotifier {
-  final List<FormfieldModel> _fields = [];
+// class FormProvider extends ChangeNotifier {
+//   final List<FormfieldModel> _fields = [];
+//   final Map<String?, TextEditingController> _controllers = {};
 
-  List<FormfieldModel> get fields => _fields;
+//   List<FormfieldModel> get fields => _fields;
+//   Map<String?, TextEditingController> get controllers => _controllers;
 
-  void setFields(List<FormfieldModel> fields) {
-    _fields.clear();
-    _fields.addAll(fields);
-    notifyListeners();
-  }
+//   void setFields(List<FormfieldModel> fields) {
+//     _fields.clear();
+//     _controllers.clear();
+//     _fields.addAll(fields);
 
-  void updateFieldValue(String fieldID, String value) {
-    final field = _fields.firstWhere((field) => field.fieldId == fieldID);
-    field.fieldValue = value;
-    notifyListeners();
-  }
-}
+//     // Initialize controllers for each field
+//     for (var field in fields) {
+//       if (field.fieldType == "InputBox") {
+//         _controllers[field.fieldId] =
+//             TextEditingController(text: field.fieldValue);
+//       }
+//     }
+//     notifyListeners();
+//   }
+
+//   void updateFieldValue(String fieldID, String value) {
+//     final field = _fields.firstWhere((field) => field.fieldId == fieldID);
+//     field.fieldValue = value;
+
+//     // Update controller value
+//     if (_controllers.containsKey(fieldID)) {
+//       _controllers[fieldID]!.text = value;
+//     }
+//     notifyListeners();
+//   }
+
+//   void disposeControllers() {
+//     // Dispose all controllers
+//     _controllers.values.forEach((controller) => controller.dispose());
+//   }
+// }
